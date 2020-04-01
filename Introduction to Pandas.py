226b797f57fe3f1e1c63faabce9ea91e49ba7ecb
c.maldonado424@gmail.com <cmaldonado@avsllc.local>
2020-04-01 11:54:32 -0400

    initial commit

---
+++ Introduction to Pandas.py
@@ -0,0 +1,163 @@
+# Create a DataFrame
+df1 = pd.DataFrame({
+  'Product ID': [1, 2, 3, 4],
+  # add Product Name and Color here
+  'Product Name': ['t-shirt', 't-shirt', 'skirt', 'skirt'],
+  'Color': ['blue', 'green', 'red', 'black']
+})
+
+print(df1)
+
+# Create a DataFrame
+df2 = pd.DataFrame([
+  [1, 'San Diego', 100],
+  [2, 'Los Angeles', 120],
+  [3, 'San Francisco', 90],
+  [4, 'Sacramento', 115]
+],
+  columns=['Store ID', 'Location', 'Number of Employees'
+  ])
+
+print(df2)
+
+#csv 
+name,cake_flavor,frosting_flavor,topping
+Devil'sFood,chocolate,chocolate,chocolateshavings
+Birthday Cake,vanilla,vanilla,rainbow sprinkles
+Carrot cake,carrot,cream cheese,almonds
+
+#Selecting Columns
+df = pd.DataFrame([
+  ['January', 100, 100, 23, 100],
+  ['February', 51, 45, 145, 45],
+  ['March', 81, 96, 65, 96],
+  ['April', 80, 80, 54, 180],
+  ['May', 51, 54, 54, 154],
+  ['June', 112, 109, 79, 129]],
+  columns=['month', 'clinic_east',
+           'clinic_north', 'clinic_south',
+           'clinic_west']
+)
+
+clinic_north = df.clinic_north
+
+print(type(clinic_north))
+
+#Selecting Multiple columns
+df = pd.DataFrame([
+  ['January', 100, 100, 23, 100],
+  ['February', 51, 45, 145, 45],
+  ['March', 81, 96, 65, 96],
+  ['April', 80, 80, 54, 180],
+  ['May', 51, 54, 54, 154],
+  ['June', 112, 109, 79, 129]],
+  columns=['month', 'clinic_east',
+           'clinic_north', 'clinic_south',
+           'clinic_west']
+)
+
+clinic_north_south = df[['clinic_north', 'clinic_south']]
+
+print(type(clinic_north_south))
+
+#Selecting a Row 
+df = pd.DataFrame([
+  ['January', 100, 100, 23, 100],
+  ['February', 51, 45, 145, 45],
+  ['March', 81, 96, 65, 96],
+  ['April', 80, 80, 54, 180],
+  ['May', 51, 54, 54, 154],
+  ['June', 112, 109, 79, 129]],
+  columns=['month', 'clinic_east',
+           'clinic_north', 'clinic_south',
+           'clinic_west'])
+
+march=df.iloc[2]
+
+#Selecting Multiple Rows
+df = pd.DataFrame([
+  ['January', 100, 100, 23, 100],
+  ['February', 51, 45, 145, 45],
+  ['March', 81, 96, 65, 96],
+  ['April', 80, 80, 54, 180],
+  ['May', 51, 54, 54, 154],
+  ['June', 112, 109, 79, 129]],
+  columns=['month', 'clinic_east',
+           'clinic_north', 'clinic_south',
+           'clinic_west']
+)
+
+april_may_june = df.iloc[3:7]
+print(april_may_june)
+
+#Selecting Rows with logic
+df = pd.DataFrame([
+  ['January', 100, 100, 23, 100],
+  ['February', 51, 45, 145, 45],
+  ['March', 81, 96, 65, 96],
+  ['April', 80, 80, 54, 180],
+  ['May', 51, 54, 54, 154],
+  ['June', 112, 109, 79, 129]],
+  columns=['month', 'clinic_east',
+           'clinic_north', 'clinic_south',
+           'clinic_west'])
+
+january = df[df.month == 'January']
+print(january)
+
+#Selecting Multiple Rows with Logic | = or, &= and 
+df = pd.DataFrame([
+  ['January', 100, 100, 23, 100],
+  ['February', 51, 45, 145, 45],
+  ['March', 81, 96, 65, 96],
+  ['April', 80, 80, 54, 180],
+  ['May', 51, 54, 54, 154],
+  ['June', 112, 109, 79, 129]],
+  columns=['month', 'clinic_east',
+           'clinic_north', 'clinic_south',
+           'clinic_west'])
+
+march_april=df[(df.month=='March')|
+  (df.month=='April')]
+
+print(march_april)
+
+#Selecting Multiple Rows with Logic: isin command checks against a list of OR's
+df = pd.DataFrame([
+  ['January', 100, 100, 23, 100],
+  ['February', 51, 45, 145, 45],
+  ['March', 81, 96, 65, 96],
+  ['April', 80, 80, 54, 180],
+  ['May', 51, 54, 54, 154],
+  ['June', 112, 109, 79, 129]],
+  columns=['month', 'clinic_east',
+           'clinic_north', 'clinic_south',
+           'clinic_west'])
+
+january_february_march=df[df.month.isin(['January', 'February', 'March'])]
+
+print(january_february_march)
+
+#Setting Indices; reset.index creates new consecutive index; inplace=True modifies existing DF 
+# drop=True drops the old index column 
+df = pd.DataFrame([
+  ['January', 100, 100, 23, 100],
+  ['February', 51, 45, 145, 45],
+  ['March', 81, 96, 65, 96],
+  ['April', 80, 80, 54, 180],
+  ['May', 51, 54, 54, 154],
+  ['June', 112, 109, 79, 129]],
+  columns=['month', 'clinic_east',
+           'clinic_north', 'clinic_south',
+           'clinic_west']
+)
+
+df2 = df.loc[[1, 3, 5]]
+
+print(df2)
+
+df3 = df2.reset_index(inplace=True, drop=True)
+
+
+
+
